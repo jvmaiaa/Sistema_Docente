8f9afb1ebf562b93c854645b3913809995a14844
jvmaiaa <joaovictormaia482@gmail.com>
2024-09-11 22:52:00 -0300

    Esqueleto do projeto Criado

---
+++ src/main/java/com/docente/sistema_docente/SistemaDocenteApplication.java
@@ -0,0 +1,13 @@
+package com.docente.sistema_docente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDocenteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaDocenteApplication.class, args);
+    }
+
+}
